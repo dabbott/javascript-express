afa051b47c59428328ce7497dffa1e682bf49134
Devin Abbott <devinabbott@gmail.com>
2020-06-03 12:00:09 -0700

    Improve slides

--- pages/syntax/slides.js
+++ pages/syntax/slides.js
@@ -9,6 +9,7 @@ export default () => (
       require('!!babel-loader!spectacle-mdx-loader!./equality.mdx'),
       require('!!babel-loader!spectacle-mdx-loader!./logical_operators.mdx'),
       require('!!babel-loader!spectacle-mdx-loader!./variables.mdx'),
+      require('!!babel-loader!spectacle-mdx-loader!./iteration.mdx'),
     ]}
   />
 )
--- pages/types/slides.js
+++ pages/types/slides.js
@@ -8,7 +8,6 @@ export default () => (
       require('!!babel-loader!spectacle-mdx-loader!./index.mdx'),
       require('!!babel-loader!spectacle-mdx-loader!./primitive_types.mdx'),
       require('!!babel-loader!spectacle-mdx-loader!./object_types.mdx'),
-      require('!!babel-loader!spectacle-mdx-loader!./library_types.mdx'),
       require('!!babel-loader!spectacle-mdx-loader!./nullability.mdx'),
       require('!!babel-loader!spectacle-mdx-loader!./checking_types.mdx'),
     ]}
