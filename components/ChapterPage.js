cf3867fa73cdb59a40c1c60b6b4375c9cf48c774
Devin Abbott <devinabbott@gmail.com>
2020-06-09 19:40:15 -0700

    Use SFC for ChapterPage. Remove HideAt and ShowAt

--- components/ChapterPage.js
+++ components/ChapterPage.js
@@ -1,7 +1,7 @@
 import { MDXProvider } from '@mdx-js/react'
 import { withRouter } from 'next/router'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
 import mediaQuery from '../utils/mediaQuery'
@@ -55,6 +55,24 @@ const SidebarContainer = styled.div({
   minWidth: '0',
   minHeight: '0',
   outline: 'none',
+
+  [mediaQuery.small]: {
+    display: 'none',
+  },
+})
+
+const MobileOnly = styled.div({
+  display: 'none',
+  [mediaQuery.small]: {
+    display: 'flex',
+  },
+})
+
+const DesktopOnly = styled.div({
+  display: 'flex',
+  [mediaQuery.small]: {
+    display: 'none',
+  },
 })
 
 const MenuContainer = styled.div({
@@ -91,113 +109,86 @@ const GithubRibbon = ({ title }) => (
   </a>
 )
 
-class ChapterPage extends React.Component {
-  state = {
-    showSidebar: true,
-    showMenu: false,
-  }
+const ChapterPage = ({ router, children }) => {
+  const [showSidebar, setShowSidebar] = useState(true)
+  const [showMenu, setShowMenu] = useState(false)
 
-  toggleSidebar = () => {
-    const { showSidebar } = this.state
+  useEffect(
+    () => {
+      setShowMenu(false)
+    },
+    [router.pathname]
+  )
 
-    this.setState({ showSidebar: !showSidebar })
-  }
+  const slug = router.pathname.slice(1)
 
-  toggleMenu = () => {
-    const { showMenu } = this.state
+  const isIntroduction = slug === ''
+  const section = getSection(slug)
 
-    this.setState({ showMenu: !showMenu })
-  }
+  if (!section) return `Could not find page: ${slug}`
 
-  // Hide the menu on mobile after clicking a link
-  componentWillReceiveProps(nextProps) {
-    const { showMenu } = this.state
+  const title = section.fullTitle || section.title
+  const nextSection = getNextSection(slug)
+  const previousSection = getPreviousSection(slug)
 
-    if (showMenu && nextProps.router.pathname !== this.props.router.pathname) {
-      this.setState({ showMenu: false })
-    }
-  }
+  const footer = (
+    <Footer>
+      <NavigatorButton
+        nextSection={nextSection}
+        previousSection={previousSection}
+      />
+    </Footer>
+  )
 
-  render() {
-    const { children } = this.props
-    const { showSidebar, showMenu } = this.state
+  const contents = (
+    <MDXProvider components={PageComponents}>{children}</MDXProvider>
+  )
 
-    const slug = this.props.router.pathname.slice(1)
-
-    const isIntroduction = slug === ''
-    const section = getSection(slug)
-
-    if (!section) return `Could not find page: ${slug}`
-
-    const title = section.fullTitle || section.title
-    const nextSection = getNextSection(slug)
-    const previousSection = getPreviousSection(slug)
-
-    const footer = (
-      <Footer>
-        <NavigatorButton
-          nextSection={nextSection}
-          previousSection={previousSection}
-        />
-      </Footer>
-    )
-
-    const contents = (
-      <MDXProvider components={PageComponents}>{children}</MDXProvider>
-    )
-
-    return (
-      <>
-        <Helmet title={title}>
-          <html lang="en" />
-        </Helmet>
-        <Container>
-          <Inner>
-            {showSidebar && (
-              <HideAt
-                style={{
-                  flex: '0 0 280px',
-                }}
-                breakpoint="small"
-                flex
-              >
-                <SidebarContainer>
-                  <Sidebar currentSection={section} />
-                </SidebarContainer>
-              </HideAt>
-            )}
-            <Content key={slug}>
-              <MenuButtonContainer>
-                <HideAt breakpoint="small">
-                  <HamburgerButton onPress={this.toggleSidebar} />
-                </HideAt>
-                <ShowAt breakpoint="small">
-                  <HamburgerButton onPress={this.toggleMenu} />
-                </ShowAt>
-              </MenuButtonContainer>
-              {isIntroduction ? (
-                <Page title={'JavaScript Express'} footer={footer} showLogo>
-                  <GithubRibbon title={'View on GitHub'} />
-                  {contents}
-                </Page>
-              ) : (
-                <Page title={title} footer={footer}>
-                  {contents}
-                </Page>
-              )}
-            </Content>
-          </Inner>
-          {showMenu && (
-            <ShowAt breakpoint="small">
-              <MenuContainer tabIndex="-1">
-                <Sidebar currentSection={section} centered />
-              </MenuContainer>
-            </ShowAt>
+  return (
+    <>
+      <Helmet title={title}>
+        <html lang="en" />
+      </Helmet>
+      <Container>
+        <Inner>
+          {showSidebar && (
+            <SidebarContainer>
+              <Sidebar currentSection={section} />
+            </SidebarContainer>
           )}
-        </Container>
-      </>
-    )
-  }
+          <Content key={slug}>
+            <MenuButtonContainer>
+              <DesktopOnly>
+                <HamburgerButton
+                  onPress={() => void setShowSidebar(!showSidebar)}
+                />
+              </DesktopOnly>
+              <MobileOnly>
+                <HamburgerButton onPress={() => void setShowMenu(!showMenu)} />
+              </MobileOnly>
+            </MenuButtonContainer>
+            {isIntroduction ? (
+              <Page title={'JavaScript Express'} footer={footer} showLogo>
+                <GithubRibbon title={'View on GitHub'} />
+                {contents}
+              </Page>
+            ) : (
+              <Page title={title} footer={footer}>
+                {contents}
+              </Page>
+            )}
+          </Content>
+        </Inner>
+        {showMenu && (
+          <MobileOnly>
+            <MenuContainer tabIndex="-1">
+              <Sidebar currentSection={section} centered />
+            </MenuContainer>
+          </MobileOnly>
+        )}
+      </Container>
+    </>
+  )
 }
 
 ChapterPage.propTypes = {
