51142bee8061fdf62a6245b6871bb52b02e1f28c
Devin Abbott <devinabbott@gmail.com>
2020-09-24 15:11:30 -0700

    Update playgrounds

--- pages/playground.js
+++ pages/playground.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import EditorConsole from '../components/EditorConsole'
 
+const getCodeParameter = () => {
+  const params = window.location.search.slice(1).split('&')
+
+  for (let param of params) {
+    const [key, value] = param.split('=')
+
+    if (key === 'code') {
+      return decodeURIComponent(value)
+    }
+  }
+
+  return ''
+}
+
 export default function Playground() {
+  const [ready, setReady] = useState(false)
+
+  useEffect(() => {
+    setReady(true)
+
+    window.onmessage = e => {
+      const data = JSON.parse(e.data)
+      history.replaceState({}, '', `?code=${encodeURIComponent(data.code)}`)
+    }
+  }, [])
+
+  const code = useMemo(() => (ready ? getCodeParameter() : ''), [ready])
+
+  if (!ready) return null
+
   return (
     <EditorConsole
       height={'100vh'}
-      code={`console.log('Hello, playgrounds!')`}
+      code={code || `console.log('Hello, playgrounds!')`}
       sharedEnvironment={true}
+      playground={{
+        enabled: true,
+        inspector: 'node',
+        renderReactElements: true,
+        debounceDuration: 200,
+        instrumentExpressionStatements: true,
+      }}
+      targetOrigin={'*'}
     />
   )
 }
